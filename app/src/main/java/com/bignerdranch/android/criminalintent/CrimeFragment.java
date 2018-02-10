b599585479a9f1d86de67cac183901fbeaf8b2c8
Sajjad Patel <sajjadp92@outlook.com>
2018-02-10 10:27:16 -0500

    Refactored activity_crime.xml and started working on fragment_crime.xml

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sajjadpatel on 2018-02-10.
+ */
+
+public class CrimeFragment extends Fragment {
+    Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
